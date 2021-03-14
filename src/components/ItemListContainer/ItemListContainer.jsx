41008d3b3afdd0b811aca45c2c74625594382ae4
Fabio Roldan <fabiocroldan@gmail.com>
2021-03-14 10:59:22 -0300

    Adding the itemCount component

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import "./ItemListContainer.css";
+import ItemCount from "../ItemCount/ItemCount";
 
-class ItemListContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            itemList: []
-        }
-    }
-    render() {
-        // const items = this.state.itemList.map(t => {
-        //     <Item valor={t} />
-        // });
-        return (
-            <div className="item-list-container">
-                <h3>
-                    {this.props.greeting}
-                </h3>
-            </div>
-        )
-    }
+function ItemListContainer(props) {
+    const [currentStock, setCurrentStock] = useState(5);
+    const [cart, setCart] = useState(0);
+
+    const addToCart = (e, stock) => {
+        e.preventDefault();
+        setCurrentStock(() => setCurrentStock(currentStock - stock));
+        setCart(cart + stock);
+    };
+
+    return (
+        <div className="item-list-container">
+            <h3>
+                {props.greeting}
+            </h3>
+            <h3>
+                Items in cart: {cart}
+            </h3>
+            <ItemCount stock={currentStock} initial={0} onAdd={addToCart} />
+        </div>
+    )
+
 };
 
 export default ItemListContainer;
