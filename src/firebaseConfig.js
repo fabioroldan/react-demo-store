0d7615f24b98cf17f9ee2de2b8bbb106866e701d
Fabio Roldan <fabiocroldan@gmail.com>
2021-04-14 22:30:20 -0300

    Added firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD82wEzw9YrSb3Y8U-KlOrAwdUHp3ZudF4",
+  authDomain: "react-demo-store.firebaseapp.com",
+  databaseURL: "https://react-demo-store-default-rtdb.firebaseio.com",
+  projectId: "react-demo-store",
+  storageBucket: "react-demo-store.appspot.com",
+  messagingSenderId: "509871092391",
+  appId: "1:509871092391:web:72c42e630b44fbdd736182",
+  measurementId: "G-BM4NPJBGC6",
+};
+
